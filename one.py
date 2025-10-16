cb821e2fbe33dc43390a535319d52164435908bb
Turab031 <147339712+Turab031@users.noreply.github.com>
2025-10-16 23:39:13 +0530

    first commit

---
+++ one.py
@@ -0,0 +1,17 @@
+import numpy as np
+a=np.array([[1,2,3],
+            [3,4,5]])
+# print(a.shape)
+
+a=np.array([1,2,3,4,15,89])
+# # slicing from start
+# print(a[:3])
+# # alicing from end
+# print(a[3:])
+
+# print(np.zeros((6,3,4)))
+
+
+arr = ([2,3,78,68])
+# print(np.sort(arr))
+print(np.argsort(arr))
